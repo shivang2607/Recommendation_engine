8364fdf197aac41733754f4f339a73ab7388c180
Shivang <Shivangkh26@gmail.com>
2023-01-29 15:47:02 +0530

    header allowed cross origin request

--- main.py
+++ main.py
@@ -5,7 +5,25 @@ import json
 import joblib
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 train = joblib.load('train.sav')
 train_combined = joblib.load('train_combined.sav')
 anime = pd.read_parquet('final_anime_list.parquet')
@@ -26,6 +44,10 @@ class title(BaseModel):
 
 class recom(title):
     recommendations:dict
+
+@app.get("/")
+async def main():
+    return {"message": "Hare Krishna Hare Krishna, Krishna Krishna Hare Hare, Hare Ram Hare Ram, Ram Ram Hare Hare"}
 
 @app.post('/anime')
 def recommend(input:title):
